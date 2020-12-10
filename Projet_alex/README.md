a5ac9e2b996291f7ad6a04c8afea95e52387c13c
MultiTec1 <mael190399@hotmail.fr>
2020-12-10 16:39:29 +0100

    rangement

---
+++ Projet_alex/README.md
@@ -0,0 +1,2 @@
+# SUPERVENTES
+Projet de programmation d'un site web de e-commerce, utilisant l'architecture MEAN.
---
+++ Projet_alex/creationBase.sh
@@ -0,0 +1,3 @@
+mongoimport --drop --db SUPERVENTES --collection users --file users.json --jsonArray
+mongoimport --drop --db SUPERVENTES --collection produits --file produits.json --jsonArray 
+mongoimport --drop --db SUPERVENTES --collection paniers --file paniers.json --jsonArray 
