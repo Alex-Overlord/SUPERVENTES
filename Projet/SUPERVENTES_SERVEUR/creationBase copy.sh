998143473321034627e22d7aeefc731e4c8dce17
Alex <canton.alex@live.fr>
2020-12-10 17:04:51 +0100

    npm

--- Projet/SUPERVENTES_SERVEUR/creationBase copy.sh
+++
@@ -1,3 +0,0 @@
-mongoimport --drop --db SUPERVENTES --collection users --file users.json --jsonArray
-mongoimport --drop --db SUPERVENTES --collection produits --file produits.json --jsonArray 
-mongoimport --drop --db SUPERVENTES --collection paniers --file paniers.json --jsonArray 
