80b1d107e33d257c2d829d36b07db458930f2bd3
Alex <canton.alex@live.fr>
2020-12-10 17:57:27 +0100

    remove saut de ligne

--- Projet/SUPERVENTES_SERVEUR/creationBase.sh
+++ Projet/SUPERVENTES_SERVEUR/creationBase.sh
@@ -1,3 +1,3 @@
 mongoimport --drop --db SUPERVENTES --collection membres --file membres.json --jsonArray
-mongoimport --drop --db SUPERVENTES --collection produits --file produits.json --jsonArray 
-mongoimport --drop --db SUPERVENTES --collection paniers --file paniers.json --jsonArray 
+mongoimport --drop --db SUPERVENTES --collection produits --file produits.json --jsonArray
+mongoimport --drop --db SUPERVENTES --collection paniers --file paniers.json --jsonArray
