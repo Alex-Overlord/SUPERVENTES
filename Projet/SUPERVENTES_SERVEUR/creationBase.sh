b53c845a5e45a31ba185c6cdaac190d781d4aadc
MultiTec1 <mael190399@hotmail.fr>
2020-12-16 01:47:16 +0100

    ménage

--- Projet/SUPERVENTES_SERVEUR/creationBase.sh
+++
@@ -1,3 +0,0 @@
-mongoimport --drop --db SUPERVENTES --collection membres --file membres.json --jsonArray
-mongoimport --drop --db SUPERVENTES --collection produits --file produits.json --jsonArray
-mongoimport --drop --db SUPERVENTES --collection paniers --file paniers.json --jsonArray
